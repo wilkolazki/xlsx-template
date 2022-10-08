6e6dd54f5dc5e8ff7067d1d5dcb9a1f840a5d1b2
Szymon Wilkołazki <szymon.wilkolazki@exclusive-networks.pl>
2022-10-08 12:06:24 +0200

    Small fixes in format-xml.sh
    - check if any *.xlsx exists in the current directory
    - create `extract` directory with `-p` option, so no warning is shown if the directory already exists
    - check if target directory exists before trying to remove it

--- format-xml.sh
+++ format-xml.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Helper script to extract test files to make them easier to visually inspect
+ls -q  -- *.xlsx > /dev/null || exit 1
 
-mkdir extract
+mkdir -p extract
 
 for x in *.xlsx ; do
 
-    rm -r extract/$x
+    test -d extract/$x && rm -r extract/$x
     mkdir extract/$x
     cd extract/$x
     unzip ../../$x
